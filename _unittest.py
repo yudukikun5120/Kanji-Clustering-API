fedba4405d5c6c03b118ba393c0863819767d496
Yuzuki Arai <yudukikun5120@gmail.com>
2022-06-20 16:00:53 +0900

    refactor: improve flexibility for character sets

--- _unittest.py
+++ _unittest.py
@@ -1,11 +1,17 @@
 import unittest
-from clustering import *
+from clustering import kanji_group
+from affinities_detection import *
 
 
 class TestOutput(unittest.TestCase):
+    
+    def test_kanji_jis_daiichisuijun(self):
+        self.assertEqual(len(kanji_group('jis_level_1')), 2965)
+
 
     def test_store_estimator(self):
         store_estimator()
+
 
     def test_affinities(self):
         characters = ["蟻", "宕", "曜"]
--- clustering.py
+++ clustering.py
@@ -14,41 +14,46 @@ progressbar.streams.wrap_stderr()
 logging.basicConfig(level=logging.INFO)
 
 
-def kanji_jis_daiichisuijun() -> list:
-    kanji_list = []
-    for first in range(0xB0A0, 0xCFA0 + 0x100, 0x100):
-        last = first + 0x10 * 6
-        for code in range(first, last):
-            if code > 0xCFD3 or not character_present(code):
-                continue
-            byte = int.to_bytes(code, byteorder='big', length=2)
-            char = byte.decode('euc_jisx0213', errors='ignore')
-            kanji_list.append(char)
+def kanji_group(set: str) -> list:
+    def attrs(set: str) -> dict:
+        if set == 'jis_level_1':
+            return {
+                'first': 0xB0A0,
+                'end': 0xCFD3
+            }
 
-    logging.debug(kanji_list)
+    def character_present(code: int) -> bool:
+        divisor = 0x100
 
-    return kanji_list
+        return (
+            attrs(set)['first'] <= code <= attrs(set)['end']
+        ) and (
+            code % divisor not in [0xA0, 0xFF]
+        )
 
-
-def character_present(code: int) -> bool:
-    divisor = 0x100
-
-    return code % divisor not in [0xA0, 0xFF]
-
-
-def extract_feature() -> np.ndarray:
-    feature = np.array(
-        [ndarray_of(char)
-            for char in progressbar.progressbar(kanji_jis_daiichisuijun())])
-
-    feature = feature.reshape(feature.shape[0], -1)
-
-    return feature
+    return [
+        int.to_bytes(code, byteorder='big', length=2)
+        .decode('euc_jisx0213', errors='ignore')
+        for first_in_block in range(0xB0A0, 0xCFA0 + 0x100, 0x100)
+        for code in range(first_in_block, first_in_block + 0x10 * 6)
+        if character_present(code)
+    ]
 
 
-def create_fitted_estimator(n_clusters: int = 100) -> (KMeans, pd.DataFrame):
+def extract_feature(set: str) -> np.ndarray:
+
+    return np.array(
+        [
+            ndarray_of(char)
+            for char in progressbar.progressbar(kanji_group(set))
+        ]).reshape(feature.shape[0], -1)
+
+
+def create_fitted_estimator(set: str,
+                            n_clusters: int = 100) -> (KMeans,
+                                                       pd.DataFrame):
     logging.info("extracting feature values...")
-    feature = extract_feature()
+    feature = extract_feature(set)
 
     logging.info("fitting an estimator with k-means clustering...")
     estimator = KMeans(n_clusters=n_clusters).fit(feature)
@@ -58,7 +63,7 @@ def create_fitted_estimator(n_clusters: int = 100) -> (KMeans, pd.DataFrame):
 
     df = pd.concat(
         [pd.Series(
-            kanji_jis_daiichisuijun(),
+            kanji_group(set),
             name="character"), labels], axis=1)
 
     logging.debug("""The estimator dataframe:
@@ -68,8 +73,8 @@ def create_fitted_estimator(n_clusters: int = 100) -> (KMeans, pd.DataFrame):
     return estimator, df
 
 
-def store_estimator():
-    estimator, df = create_fitted_estimator()
+def store_estimator(set: str):
+    estimator, df = create_fitted_estimator(set)
 
-    with open('estimator/JIS_level-1_kanji_set.pkl', 'wb') as f:
+    with open(f'estimator/{set}_set.pkl', 'wb') as f:
         pickle.dump((estimator, df), f)
